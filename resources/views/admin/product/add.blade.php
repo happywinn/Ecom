563842a707338a5a916e23ea22124c2e607bf021
happywinn <sutaung2030@gmail.com>
2022-08-12 00:53:49 +0630

    change11 product brand_id

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -11,7 +11,7 @@
 				<div class="row">
 					<div class="col-md-12 mb-3">
 						<select class="form-select" name="cate_id">
-							<option></option>
+							<option value=null></option>
 							@foreach($category as $item)
 							<option value="{{$item->id}}">
 								{{$item->name}}
@@ -30,7 +30,7 @@
 					<div class="col-md-4 mb-3">
 						<label for="">Brand</label>
 						<select class="form-select" name="brand_id">
-							<option></option>
+							<option value=null></option>
 							@foreach($brands as $brand)
 							<option value="{{$item->id}}">
 								{{$brand->name}}
